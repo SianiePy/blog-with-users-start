7e013905fea03e3a0d2547f12131b62e73876e5e
SianOak <sian@oakgames.co.uk>
2021-01-27 16:13:15 +0000

    This and last couple are changing database to postgres

--- main.py
+++ main.py
@@ -280,4 +280,4 @@ def delete_post(post_id):
 
 if __name__ == "__main__":
     app.run()
-    # app.run(host='0.0.0.0', port=5000)   #change to app.run() line above for local work
+    # app.run(host='0.0.0.0', port=5000)   #change to app.run() line above for local work so
